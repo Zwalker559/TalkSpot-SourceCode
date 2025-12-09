3d1b1ede7ca4c1b533a2e7c87b3dcf1fa69175cb
Zwalker559 <zacharywalker013@gmail.com>
2025-12-09 06:47:46 +0000

    Try fixing this error: `Runtime Error: Error: A "use server" file can on

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,4 +1,4 @@
 
 // Flows will be imported for their side effects in this file.
 import './flows/translate-flow';
-import './flows/reset-password-flow';
+// The reset-password-flow is now a standard server action, no need to import here for Genkit dev.
